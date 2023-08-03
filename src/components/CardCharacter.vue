2bfb63d9947dfab11f454e1d2ebeb204e6bdee20
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-08-02 18:27:40 -0600

    aagregamos location and origin

--- src/components/CardCharacter.vue
+++ src/components/CardCharacter.vue
@@ -3,6 +3,27 @@
     <img :src="character.image" alt="character.name" />
     <div class="character__info">
       <h3>{{ character.name }}</h3>
+      <div class="status">
+        <span
+          :class="
+            character.status == 'Alive'
+              ? 'alive'
+              : character.status == 'Dead'
+              ? 'dead'
+              : 'default'
+          "
+        ></span>
+        <span>{{ character.status }}- {{ character.species }}</span>
+        <div class="origin">
+          <span>Origin: {{ character.origin.name }}</span>
+        </div>
+
+        <div class="location">
+          <div class="origin">
+            <span>Location: {{ character.location.name }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
