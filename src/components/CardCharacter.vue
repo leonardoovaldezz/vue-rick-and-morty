76b61c770dc63b5de15114d04fb8786ada835623
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-08-02 18:15:05 -0600

    listado objeto personajes

---
+++ src/components/CardCharacter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="character">
+    <div class="character__info">
+      {{ character.name }}
+    </div>
+  </div>
+</template>
+  
+  <script>
+export default {
+  props: ["character"],
+};
+</script>
+  
+  <style>
+</style>
+  
